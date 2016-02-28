93fa2b9258837ccdf84500ae3ccc14f5f3ebd4a7
Tatsiana Clifton <tatsianaclifton@gmail.com>
2016-02-27 22:40:02 -0500

    need to fix bg processes

--- Small Shell/smallsh.c
+++ Small Shell/smallsh.c
@@ -28,17 +28,50 @@
 
 #define MAXSIZE 2048 //used limit the lenght of a command 
 #define MAXARG 512 //used to limit number of arguments  
-#define DELIM " \t\r\n\a"
+#define DELIM " \n\t\r\a"
+
+int signo;
 
 void sigHandler(){
-   struct sigaction act;
-   act.sa_handler = SIG_IGN;
-   sigaction(SIGINT, &act, NULL);
+   int status;
+   int signo;
+   pid_t cpid;
+   char msg[100]; 
+   char sigNum[5];
+   char statusNum[3];
+   //for all child processes
+   while((cpid = waitpid(-1, &status, WNOHANG)) > 0){
+      char pid[15];
+      snprintf(pid, 15, "%i", cpid);
+
+      strncpy(msg, "background pid ", 100);
+      strcat(msg, pid);
+      strcat(msg, " is done:  ");
+      //if a child procces is terminated by signal
+      if (WIFSIGNALED(status)){
+          signo = WTERMSIG(status);
+          snprintf(sigNum, 5, "%i", signo);
+          strcat(msg, "terminated by signal ");
+          strcat(msg, sigNum);
+          strcat(msg, "\n");
+          write(1, msg, 100); 
+      }
+      //if a child proccess is finished normal
+      else{
+         status = WEXITSTATUS(status);
+         snprintf(statusNum, 3, "%i", status);
+         strcat(msg, "exit value ");
+         strcat(msg, statusNum);
+         strcat(msg, "\n");
+         write(1, msg, 100);    
+      }
+      continue;
+   } 
 }
 
 char *readInput(){
-   char *buffer = NULL;
    ssize_t size = MAXSIZE;
+   char *buffer = NULL;
    getline(&buffer, &size, stdin);
    return buffer; 
 }
@@ -55,16 +88,25 @@ char **getArgs(char *input){
    token = strtok (input, DELIM);
    while (token != NULL){
       argsArr[i] = token;
+      //printf("%s\n", argsArr[i]);
+      //fflush(stdout); 
       i++;
       token = strtok (NULL, DELIM); 
    }
    argsArr[i] = NULL;
+   //printf("%s\n", argsArr[i]);
+   //fflush(stdout); 
+  
    return argsArr;   
 }
 
 int execute(char **args, int status){
+   if (args[0] == NULL){
+      return 1; 
+   }
    //exit command
    if (strcmp("exit", args[0]) == 0){
+      //stop = 1;
       exit(EXIT_SUCCESS);
    }
    //cd command
@@ -80,8 +122,16 @@ int execute(char **args, int status){
    }
    //status command
    if (strcmp("status", args[0]) == 0){
-      printf("exit value %i\n", status);
-      fflush(stdout);
+      if (status == 2){
+         printf("terminated by signal %i\n", signo);
+         fflush(stdout);
+         status = 0; 
+      }
+      else{
+         printf("exit value %i\n", status);
+         fflush(stdout);
+         status = 0;
+      }   
    }
    //
    else { 
@@ -90,40 +140,45 @@ int execute(char **args, int status){
 }
 
 int executeCommand(char **args){
-   int status;
+   int status = 0;
+   int statusReturn = 0;
    pid_t cpid, wpid;
    int finput = 0;
    int foutput = 0;
    int bg = 0;
 
-   // while(args[i] != NULL){
    if (args[1] != NULL){  
       if(strcmp(args[1], "<") == 0){
-         finput = 1; 
-         //change < to NULL; then it will be ignored while execution
-         //args[i] = NULL;   
+         finput = 1;          
       }
       else if(strcmp(args[1], ">") == 0){
          foutput = 1;
-         //args[i] = NULL;
       } 
-      //else if(strcmp(args[i], "&") == 0){
-         //args[i] = NULL;
-        // bg = i;
-      //}
-      //i++;
-   //}
+   }
+
+   int i;
+   for (i = 0; args[i] != NULL; i++){
+      if ((strncmp(args[i], "&", 1)) == 0){
+          bg = 1;
+          args[i] = NULL;
+      }
    }
    
    int fd, fd2;
    if (bg == 0){
+
+       struct sigaction act;
+       act.sa_flags = 0;
+       act.sa_handler = SIG_DFL;
+       sigaction(SIGINT, &act, NULL);
+
        if (finput == 0 && foutput == 0){ 
           cpid = fork(); 
           if (cpid == 0){
              if (execvp(args[0], args) == -1){ 
                 perror("exec");
-                status = 1;
-                exit(1);
+                statusReturn = 1;
+                exit(EXIT_FAILURE);
              }            
           }
           else if (cpid < 0){
@@ -131,9 +186,26 @@ int executeCommand(char **args){
              return 1;
           }
           else{
-             do{
+             //handler for parent proccess to ignore CTRL-C
+             struct sigaction act;
+             act.sa_handler = SIG_DFL;
+             act.sa_handler = SIG_IGN;
+             sigaction(SIGINT, &act, NULL);   
+             /*do{
+                //wait for chil proccess to finish
                 wpid = waitpid(cpid, &status, WUNTRACED);
-             }while (!WIFEXITED(status) && !WIFSIGNALED(status));
+             }while (!WIFEXITED(status) && !WIFSIGNALED(status));*/
+
+             waitpid(cpid, &status, 0);
+             statusReturn = WEXITSTATUS(status);
+
+             //if terminated by signal show termination message
+             if(WIFSIGNALED(status)){
+                signo = WTERMSIG(status);
+                printf("terminated by signal %i\n", signo);
+                statusReturn = 2;
+             }
+             //status = 1; 
           } 
        }
        //redirect standart input
@@ -144,30 +216,41 @@ int executeCommand(char **args){
               fd = open(args[finput+1], O_RDONLY);
               if (fd == -1){
                  perror("open");
-                 status = 1;
+                 statusReturn = 1;
                  exit(EXIT_FAILURE);
               }
               fd2 = dup2(fd,0);
               if (execlp(args[0], args[0], NULL) == -1){ 
                   perror("exec");
-                  status = 1;
+                  statusReturn = 1;
+                  exit(EXIT_FAILURE);  
               }
-              exit(EXIT_FAILURE);
-                          
           }
           else if (cpid < 0){
              perror("fork()");
-             status = 1;
-             return status;
+             statusReturn = 1;
+             return statusReturn;
           }
           else{
+             //handler for parent proccess to ignore CTRL-C
+             /*struct sigaction act;
+             act.sa_handler = SIG_DFL;
+             act.sa_handler = SIG_IGN;
+             sigaction(SIGINT, &act, NULL);*/   
              do{
+                //wait for chil proccess to finish
                 wpid = waitpid(cpid, &status, WUNTRACED);
              }while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
+             //if terminated by signal show termination message
+            /* if(WIFSIGNALED(status)){
+                int signo = WTERMSIG(status);
+                printf("terminated by signal %i\n", signo);
+             }*/
           } 
           close(fd); 
       }
-
+      
       else {
          cpid = fork();
          
@@ -175,57 +258,120 @@ int executeCommand(char **args){
             fd = open(args[foutput+1], O_WRONLY|O_CREAT|O_TRUNC,0644);
             if (fd == -1){
                perror("open");
-               status = 1;
+               statusReturn = 1;
                exit(EXIT_FAILURE);
              }
              fd2 = dup2(fd,1);
           
              if (execlp(args[0], args[0], NULL) == -1){ 
                 perror("exec");
-                status = 1;
+                statusReturn = 1;
+                //exit(EXIT_FAILURE);
              }
-                exit(EXIT_FAILURE);
+             exit(EXIT_FAILURE);
           }
           else if (cpid < 0){
              perror("fork()");
-             status = 1;
-             return status;
+             statusReturn = 1;
+             return statusReturn;
           }
           else{
+             //handler for parent proccess to ignore CTRL-C
+             /*struct sigaction act;
+             act.sa_handler = SIG_DFL;
+             act.sa_handler = SIG_IGN;
+             sigaction(SIGINT, &act, NULL); */  
              do{
+                //wait for chil proccess to finish
                 wpid = waitpid(cpid, &status, WUNTRACED);
              }while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
+             //if terminated by signal show termination message
+             /*if(WIFSIGNALED(status)){
+                int signo = WTERMSIG(status);
+                printf("terminated by signal %i\n", signo);
+             } */
           } 
           close(fd);  
        }
-      
    }
-   return 1; 
+   else if (bg != 0){
+      cpid = fork();
+
+      if (finput == 1){
+         fd = open(args[finput+1], O_RDONLY, 0644);
+      }
+
+      if (foutput == 1){
+         fd = open(args[foutput+1], O_WRONLY|O_TRUNC|O_CREAT, 0644);
+      }
+
+      else{
+         fd = open("/dev/null", O_RDONLY);
+      }
+
+      if (cpid == 0){      
+        // fd = open("/dev/null", O_RDONLY);
+         //if (fd == -1){
+           // perror("open");
+            //statusReturn = 1;
+            //exit(EXIT_FAILURE);
+        // }
+         //else{
+            fd2 = dup2(fd, 0);
+            close(fd);
+        // }
+         if (execlp(args[0],args[0],args[1], NULL) == -1){
+            perror("exec"); 
+            statusReturn = 1;
+            exit(EXIT_FAILURE);
+         }
+         //exit(EXIT_FAILURE); 
+      }
+      else if (cpid < 0){
+         perror("fork");
+         statusReturn = 1;
+         return statusReturn;
+      }
+      else{
+         printf("background pid is %i\n", cpid);
+      }       
+   }
+   return statusReturn; 
 }
 
 int main(){
    char *input;
    char **args;
    int status = 0;
-   char comment = '#';
-   char blank = ' ';
-
-   sigHandler();  
+   
+   //signal handler for child processes   
+   struct sigaction act;
+   act.sa_handler = sigHandler;
+   sigaction(SIGCHLD, &act, NULL);
 
    do{
       printf(": ");
       fflush(stdout);
       input = readInput();
+
+      //printf("Line 342\n");fflush(stdout);   
+      //if user entered nothing continue
+      //if(strcmp(input, "") == 0){
+        //  continue;
+      //} 
       //if an user entered #, it is a comment, ignore
       //if an user entered space ignore the line
-      if(input[0] == comment || input[0] == blank){
+      if(input[0] == '#' || input[0] == ' '){
          continue;
       }
+      //printf("line 352\n");fflush(stdout);
       args = getArgs(input);
       status = execute(args, status);
 
       free(input);
       free(args);
    }while(1);
+
    return EXIT_SUCCESS;
 }               
